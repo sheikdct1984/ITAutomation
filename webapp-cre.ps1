942d998ea2619ee71ca2d04ce6940c483610da9e
sheikdct1984 <44596391+sheikdct1984@users.noreply.github.com>
2018-10-30 09:39:44 +0530

    Update webapp-cre.ps1

---
+++ webapp-cre.ps1
@@ -0,0 +1,24 @@
+﻿$AppSeriveName = "Sheik-app-plan"
+$Location="westus"
+$resourceGroupName="sheik-rg-test1"
+$size="b1"
+$webappnameapi="Sheik-webapp1"
+
+New-AzureRmAppServicePlan -Name $AppSeriveName -Location $Location -ResourceGroupName $resourceGroupName -Tier $size
+
+New-AzureRmWebApp -Name $webappnameapi -Location $location -AppServicePlan $AppSeriveName -ResourceGroupName $resourceGroupName
+ 
+Set-AzureRmWebApp -Name $webappnameapi -ResourceGroupName $resourceGroupName -Use32BitWorkerProcess $False
+
+Set-AzureRmWebApp -Name $webappnameapi -ResourceGroupName $resourceGroupName -WebSocketsEnabled $true
+
+$website = Get-AzureRmWebApp -Name $webappnameapi -ResourceGroupName $resourceGroupName 
+$website.SiteConfig.AlwaysOn=$true
+$website | Set-AzureRmWebApp
+
+$VDApp1 = New-Object Microsoft.Azure.Management.WebSites.Models.VirtualApplication 
+$VDApp1.VirtualPath = "/Shiptech10.Api.Admin"
+$VDApp1.PhysicalPath = "site\wwwroot\Shiptech10.Api.Admin"
+$VDApp1.PreloadEnabled ="YES"
+$website.siteconfig.VirtualApplications.Add($VDApp1) 
+$website | Set-AzureRmWebApp
